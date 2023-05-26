b6fce0ced624c1f4d0ca35d5d2e61a5bf93dfb8f
Lucca Rauédys <luccarauedys@outlook.com>
2023-05-25 22:33:08 -0300

    tratando erro de video indisponivel

--- script.py
+++ script.py
@@ -1,25 +1,38 @@
 from pytube import YouTube, Playlist
+from pytube.exceptions import VideoUnavailable
 
 option = input('''O que deseja fazer?
 - Digite V para baixar um VIDEO
 - Digite P para baixar uma PLAYLIST
+
 ''')
 
 if option.upper() == 'V':
-    # FAZER DOWNLOAD DE SOMENTE UM VIDEO
-    video_link = input('\nLINK DO VIDEO: ')
-    video = YouTube(video_link)
+    video_url = input('\nLINK DO VIDEO: ')
+    try:
+        video = YouTube(video_url)
+    except VideoUnavailable:
+        print(f'Erro ao realizar download. Video indisponivel.')
+    else:
+        print(f'\nBaixando: {video.title}...')
+        video.streams.get_highest_resolution().download('./downloads/videos')
+        print('Download concluido com sucesso.')
 
-    print(f'Baixando {video.title}...')
-    video.streams.get_highest_resolution().download('./downloads/videos')
-    print('Video baixado com sucesso.')
 if option.upper() == 'P':
-    # FAZER DOWNLOAD DE TODOS OS VIDEOS DE UMA PLAYLIST
-    playlist_link = input('\nLINK DA PLAYLIST: ')
-    playlist = Playlist(playlist_link)
+    playlist_url = input('\nLINK DA PLAYLIST: ')
+    playlist = Playlist(playlist_url)
+    print(f'\nIniciando download da playlist: {playlist.title}')
 
-    print(f'Baixando: {playlist.title}...')
-    for video in playlist.videos:
-        video.streams.get_highest_resolution().download(
-            f'./downloads/playlists/{playlist.title}')
+    for video_url in playlist.video_urls:
+        try:
+            video = YouTube(video_url)
+        except VideoUnavailable:
+            print(
+                f'[Erro]: Video indisponivel em {video_url}. Pulando para o proximo...')
+        else:
+            video = YouTube(video_url)
+            print(f'Baixando video: {video.title}')
+            video.streams.get_highest_resolution().download(
+                f'./downloads/playlists/{playlist.title}')
+
     print("Playlist baixada com sucesso.")
